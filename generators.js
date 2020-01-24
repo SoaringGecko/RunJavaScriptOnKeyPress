5c87572c2f4f3d9db18e5eae47ee58c1e55bb51b
SoaringGecko <48862796+SoaringGecko@users.noreply.github.com>
2020-01-24 07:47:05 -0500

    Update generators.js

--- generators.js
+++ generators.js
@@ -76,7 +76,7 @@ function myFunction1() {
 
 function myFunction1test() {
 var x = document.getElementById("myText1").value;
-  document.getElementById("demo1").innerHTML = "<? echo htmlentities($str);?>";
+  document.getElementById("demo1").innerHTML = "<? echo htmlentities($result1);?>";
 }
 
 function myFunction2() {
